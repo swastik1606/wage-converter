64916a73679c0ea409d1ea4b540f86f496cd278a
swastik <swastik.seth@gmail.com>
2026-07-04 05:23:34 +0530

    tried fixing bugs and fixing overall logic and code

--- content.js
+++ content.js
@@ -1,32 +1,60 @@
-const PRICE_REGEX= /\$[0-9,]+(\.[0-9]{2})?)/g;
+const PRICE_REGEX = /\$\s?([0-9,]+(\.[0-9]{2})?)/g;
 
-function convertPriceToTime(userWage){
-    const walker=document.createTreeWalker(document.body.NodeFilter.SHOW_TEXT, null, false);
+let userHourlyWage = 0;
 
-    let node;
-    while (node=walker.nextNode()){
-        if (PRICE_REGEX.test(node.nodeValue)){
-            node.nodeValue=node.nodeValue.replace(PRICE_REGEX, (match,priceString)=>{
-                const cleanPriceString=priceString.replace(/ ,/g, '');
-                const price=parseFloat(clearPriceString);
+function processTextNode(node) {
+    if (node.nodeValue.includes('$')) {
+        node.nodeValue=node.nodeValue.replace(PRICE_REGEX, (match, priceString)=>{
+            const cleanPriceString=priceString.replace(/ ,/g, '');
+            const price=parseFloat(cleanPriceString);
 
-                let hours=price/userWage
+            const hours=price/userHourlyWage;
 
-                if(hours<1){
-                    const minutes=Math.round(hours*60);
-                    return `[${minutes}mins]`;
-                } else if (hours>100) {
-                    return `[${Math.round(hours)} hrs]`;
-                } else {
-                    return `[${hours.toFixed(1)} hrs]`
-                }
-            });
-        }
+            if (hours<1){
+                return `[${Math.round(hours*60)} mins]`;
+            } else if (hours >100){
+                return `[${Math.round(hours)} hrs]`;
+            } else {
+                return `[${hours.toFixed(1)} hrs]`
+            }
+        });
     }
 }
 
-chrome.storage.local.get(['hourlyWage'], (result) => {
-    if (result.hourlyWage && result.hourlyWage >0) {
-        convertPriceToTime(result.hourlyWage);
+function convertPricesInNodes(rootNode){
+    if (rootNode.nodeType===Node.TEXT_NODE){
+        processTextNode(rootNode);
+        return;
     }
-})
+
+    const walker=document.createTreeWalker(rootNode, NodeFilter.SHOW_TEXT, null, false);
+    let node;
+    while (node=walker.nextNode()){
+        processTextNode(node);
+    }
+}
+
+
+chrome.storage.local.get(['hourlyWage'], (result) => {
+    if (result.hourlyWage && result.hourlyWage > 0) {
+        userHourlyWage=result.hourlyWage;
+
+        convertPricesInNodes(document.body);
+
+        const observer=new MutationObserver((mutations)=>{
+            for (const mutation of mutations){
+                if (mutation.addedNodes.length){
+                    for (const node of mutation.addedNodes) {
+                        if (node.nodeType===Node.ELEMENT_NODE || node.nodeType === Node.TEXT_NODE){
+                            if (node.nodeName !== 'SCRIPT' && node.nodeName != 'STYLE' && node.nodeName !== 'NOSCRIPT') {
+                                convertPricesInNodes(node);
+                            }
+                        }
+                    }
+                }
+            }
+        })
+
+        observer.observe(document.body, {childList: true, subtree: true});
+    }
+});
