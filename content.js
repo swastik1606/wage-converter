86a8aa77b71bfa4e5a9829ad4c0795b000846bec
swastik <swastik.seth@gmail.com>
2026-07-04 05:33:33 +0530

    added functionality for amazon

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 const PRICE_REGEX = /\$\s?([0-9,]+(\.[0-9]{2})?)/g;
 
 let userHourlyWage = 0;
+const isAmazon=window.location.hostname.includes('amazon');
 
 function processTextNode(node) {
     if (node.nodeValue.includes('$')) {
@@ -34,12 +35,37 @@ function convertPricesInNodes(rootNode){
     }
 }
 
+function processAmazonNode(rootNode){
+    if (!rootNode.querySelectorAll) return;
+    const priceElements=rootNode.querySelectorAll('.a-price');
+
+    priceElements.forEach(el => {
+        if (el.dataset.converted) return;
+
+        const hiddenPrice = el.querySelectorAll('.a-offscreen');
+        if (hiddenPrice && hiddenPrice.textContent.includes('$')) {
+            const match= hiddenPrice.textContent.match(/\$\s?([0-9,]+(\.[0-9]{2})?)/);
+            if (match) {
+                const price=parseFloat(match[1].replace(/ ,/g, ''));
+                const hours= (price/userHourlyWage).toFixed(1);
+                
+                el.innerHTML= `<span style="color:red"; font-weight:bold;>[${hours} hrs</span>]`;
+                el.dataset.converted="true";
+            }
+        }
+    })
+}
+
 
 chrome.storage.local.get(['hourlyWage'], (result) => {
     if (result.hourlyWage && result.hourlyWage > 0) {
         userHourlyWage=result.hourlyWage;
 
-        convertPricesInNodes(document.body);
+        if (isAmazon) {
+            processAmazonNode(document.body);
+        } else {
+            convertPricesInNodes(document.body)
+        }
 
         const observer=new MutationObserver((mutations)=>{
             for (const mutation of mutations){
@@ -47,7 +73,11 @@ chrome.storage.local.get(['hourlyWage'], (result) => {
                     for (const node of mutation.addedNodes) {
                         if (node.nodeType===Node.ELEMENT_NODE || node.nodeType === Node.TEXT_NODE){
                             if (node.nodeName !== 'SCRIPT' && node.nodeName != 'STYLE' && node.nodeName !== 'NOSCRIPT') {
-                                convertPricesInNodes(node);
+                                if (isAmazon && node.nodeType === Node.ELEMENT_NODE) {
+                                    processAmazonNode(node);
+                                } else {
+                                    convertPricesInNodes(node);
+                                }
                             }
                         }
                     }
