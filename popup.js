38eff2a2debde97909e20e0f4d4467b36fe10202
swastik <swastik.seth@gmail.com>
2026-07-04 17:30:26 +0530

    added currency selector at input

--- popup.js
+++ popup.js
@@ -1,34 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
     const wageInput = document.getElementById('wageInput');
     const saveBtn = document.getElementById('saveBtn');
+    const currencySelect = document.getElementById('currencySelect')
 
-    chrome.storage.local.get(['userWages'], (result) => {
-        if (result.userWages && result.userWages['$']) {
-            wageInput.value = result.userWages['$'];
+    chrome.storage.local.get(['userWages', 'baseCurrency'], (result) => {
+        if (result.baseCurrency) {
+            currencySelect.value = result.baseCurrency;
+
+            if (result.userWages) {
+                if (result.userWages[result.baseCurrency]) wageInput.value=
+                result.userWages[result.baseCurrency];
+            }
         }
     });
 
     saveBtn.addEventListener('click', () => {
-        const baseWage = parseFloat(wageInput.value);
+        const baseWage = parseFloat(wageInput.value)
+        const baseCurrency=currencySelect.value;
 
-        if (baseWage > 0) {
-            const rates = {
-                '$': 1.00,
-                '€': 0.92,
-                '£': 0.79,
-                '₹': 95.20,
-                '¥': 155.00
-            };
+        const rates = {
+            '$': 1.00,
+            '€': 0.92,
+            '£': 0.79,
+            '₹': 95.20,
+            '¥': 155.00
+        };
 
-            const precomputedWages= {};
-            for (const symbol in rates) {
-                precomputedWages[symbol]=baseWage*rates[symbol];
-            }
+        const precomputedWages = {};
 
-            chrome.storage.local.set({userWages: precomputedWages}, ()=> {
-                saveBtn.textContent='Saved!';
-                setTimeout(()=> saveBtn.textContent='Save',1500);
-            });
+        for (const symbol in rates) {
+            precomputedWages[symbol] = (baseWage/rates[baseCurrency]) * rates[symbol];
         }
-    })
+
+        chrome.storage.local.set({
+            userWages:precomputedWages,
+            baseCurrency: baseCurrency
+        }, ()=> {
+            saveBtn.textContent='Saved!';
+            setTimeout(()=> saveBtn.textContent='Save', 1500)
+        })
+    });
 });
